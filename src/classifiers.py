c27293267b036592f4c16b1934153ec21b78be75
Darcy Cecil-Oakes <dc4216@edge26.doc.ic.ac.uk>
2018-02-19 16:33:59 +0000

    updates to q3

--- src/classifiers.py
+++ src/classifiers.py
@@ -2,32 +2,42 @@ import numpy as np
 
 
 def softmax(logits, y):
-    """
-    Computes the loss and gradient for softmax classification.
+	"""
+    	Computes the loss and gradient for softmax classification.
 
-    Args:
-    - logits: A numpy array of shape (N, C)
-    - y: A numpy array of shape (N,). y represents the labels corresponding to
-    logits, where y[i] is the label of logits[i], and the value of y have a 
-    range of 0 <= y[i] < C
+    	Args:
+    	- logits: A numpy array of shape (N, C)
+    	- y: A numpy array of shape (N,). y represents the labels corresponding to
+    	logits, where y[i] is the label of logits[i], and the value of y have a 
+    	range of 0 <= y[i] < C
 
-    Returns (as a tuple):
-    - loss: Loss scalar
-    - dlogits: Loss gradient with respect to logits
-    """
-    loss, dlogits = None, None
-    """
-    TODO: Compute the softmax loss and its gradient using no explicit loops
-    Store the loss in loss and the gradient in dW. If you are not careful
-    here, it is easy to run into numeric instability. Don't forget the
-    regularization!
-    """
-    ###########################################################################
-    #                           BEGIN OF YOUR CODE                            #
-    ###########################################################################
+    	Returns (as a tuple):
+    	- loss: Loss scalar
+    	- dlogits: Loss gradient with respect to logits
+    	"""
+    	# initalise the loss and gradient to zero
+	loss, dlogits = None, None
+	"""
+    	TODO: Compute the softmax loss and its gradient using no explicit loops
+    	Store the loss in loss and the gradient in dW. If you are not careful
+    	here, it is easy to run into numeric instability. Don't forget the
+    	regularization!
+    	"""
+    	###########################################################################
+    	#                           BEGIN OF YOUR CODE                            #
+   	###########################################################################
+	logits += -np.max (logits, axis=0)
+
+	C = np.exp(logits)
+	sum_of_C = np.sum(C, axis=0)
+
+	gradient = C/sum_of_C
+	
+	loss = np.log(sum_of_C)
 
 
-    ###########################################################################
-    #                            END OF YOUR CODE                             #
-    ###########################################################################
-    return loss, dlogits
+
+    	###########################################################################
+    	#                            END OF YOUR CODE                             #
+    	###########################################################################
+	return loss, dlogits
