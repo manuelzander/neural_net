c8c7b15503e98b084b9b6b1f612f3993f5304893
Manuel Zander <manuelzander@Manuels-MBP.connect>
2018-03-04 11:59:00 +0000

    Opimized script for testing values

--- src/train_Q5.py
+++ src/train_Q5.py
@@ -36,15 +36,16 @@ classification_rate_cache = dict()
 max_classification_rate = 0.0;
 best_method = None;
 best_model = None;
+best_solver = None;
 
-learning_rates = [1e-3, 1e-4, 1e-5, 1e-6]
+learning_rates = [1e-3, 1e-4] #1e-5, 1e-6]
 
 #Loop through learning rates specified above
 for lr in learning_rates:
     print('LEARNING RATE: %f' % lr)
 
     #Loop through learning rate decays 0.99 down to 0.90 (?)
-    for lrd in range(99, 96, -1):
+    for lrd in range(99, 95, -1):
         lrd = lrd/100
         print('LEARNING RATE DECAY: %f' % lrd)
 
@@ -70,34 +71,40 @@ for lr in learning_rates:
                 'optim_config':optim_config,
                 'lr_decay':lrd,
                 'batch_size':100,
-                'num_epochs':5,
+                'num_epochs':1,
                 'verbose': False
             }
 
+            #Create solver instances for ech model
             solver_one_layer = Solver(model_one_layer, data, **args)
             solver_two_layers = Solver(model_two_layers, data, **args)
             solver_one_layer_withdropout = Solver(model_one_layer_withdropout, data, **args)
             solver_two_layers_withdropout  = Solver(model_two_layers_withdropout, data, **args)
 
+            #Train models with solver instances and store classification rates
             solver_one_layer.train()
             classification_rate_cache['ONE_L%dNEUR%fLR_%fLRD' % (number_neurons, lr, lrd)] = solver_one_layer.best_val_acc
             if(solver_one_layer.best_val_acc > max_classification_rate):
                 best_model = solver_one_layer.model
+                best_solver = solver_one_layer
 
             solver_two_layers.train()
             classification_rate_cache['TWO_L%dNEUR%fLR_%fLRD' % (number_neurons, lr, lrd)] = solver_two_layers.best_val_acc
             if(solver_two_layers.best_val_acc > max_classification_rate):
                 best_model = solver_two_layers.model
+                best_solver = solver_two_layers
 
             solver_one_layer_withdropout.train()
             classification_rate_cache['ONE_L%dNEUR%fLR_%fLRD_DROP' % (number_neurons, lr, lrd)] = solver_one_layer_withdropout.best_val_acc
             if(solver_one_layer_withdropout.best_val_acc > max_classification_rate):
                 best_model = solver_one_layer_withdropout.model
+                best_solver = solver_one_layer_withdropout
 
             solver_two_layers_withdropout.train()
             classification_rate_cache['TWO_L%dNEUR%fLR_%fLRD_DROP' % (number_neurons, lr, lrd)] = solver_two_layers_withdropout.best_val_acc
             if(solver_two_layers_withdropout.best_val_acc > max_classification_rate):
                 best_model = solver_two_layers_withdropout.model
+                best_solver = solver_two_layers_withdropout
 
 #print(classification_rate_cache)
 print("{:<60} {:<60}".format('Method','Classification Rate'))
@@ -114,31 +121,33 @@ print('Best method: %s' % best_method)
 #######################################################################
 ### PLOT GRAPH
 #######################################################################
-'''
+
 plt.subplot(2, 1, 1)
 plt.title("Training loss")
-plt.plot(solver.loss_history, "o")
+plt.plot(best_solver.loss_history, "o")
 plt.xlabel('Iteration')
 
 plt.subplot(2, 1, 2)
 plt.title('Accuracy')
-plt.plot(solver.train_acc_history, '-o', label='train')
-plt.plot(solver.val_acc_history, '-o', label='val')
-plt.plot([0.5] * len(solver.val_acc_history), 'k--')
+plt.plot(best_solver.train_acc_history, '-o', label='train')
+plt.plot(best_solver.val_acc_history, '-o', label='val')
+plt.plot([0.5] * len(best_solver.val_acc_history), 'k--')
 plt.xlabel('Epoch')
 plt.legend(loc='lower right')
 plt.gcf().set_size_inches(15, 12)
 plt.show()
-'''
+
 #######################################################################
 ### SAVE AND LOAD MODEL
 #######################################################################
-'''
-with open('model.pkl', 'wb') as handle:
-    pickle.dump(model, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
+with open('best_model.pkl', 'wb') as handle:
+    pickle.dump(best_model, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+'''
 with open('model.pkl', 'rb') as handle:
     loaded_model = pickle.load(handle)
-'''
+
 #print("W1 after loading")
 #print(loaded_model.params['W1'])
+'''
